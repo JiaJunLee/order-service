65ccd9fc794d71cd9c03b68d44f11af56b04089f
libu <burgess.li@oocl.com>
2019-05-31 15:02:32 +0800

    migration to eureka

--- src/main/java/com/dmall/orderservice/adapter/inventory/InventoryClient.java
+++ src/main/java/com/dmall/orderservice/adapter/inventory/InventoryClient.java
@@ -1,8 +1,16 @@
 package com.dmall.orderservice.adapter.inventory;
 
-//TODO change to use feign client
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@FeignClient(name = "inventory-service")
 public interface InventoryClient {
+
+    @RequestMapping(method = RequestMethod.GET, path = "/inventories/lock")
     String lock(Lock lock);
 
-    void unlock(String lockId);
+    @RequestMapping(method = RequestMethod.PUT, path = "/inventories/lock")
+    void unlock(@PathVariable("lockId") String lockId);
 }
